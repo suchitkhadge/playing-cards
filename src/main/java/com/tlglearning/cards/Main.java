17c639a0eb42423188747a0071eb5ee80209529a
skhadge <suchit.khadge@gmail.com>
2022-09-27 14:02:42 -0700

    ColorFirst as local class in Main.

--- src/main/java/com/tlglearning/cards/Main.java
+++ src/main/java/com/tlglearning/cards/Main.java
@@ -10,6 +10,22 @@ public class Main {
 
   public static void main(String[] args) {
 
+     class ColorFirst implements Comparator<Card> {
+
+      @Override
+      public int compare(Card card1, Card card2) {
+        int comparison = card1.suit().color().compareTo(card2.suit().color());
+        if (comparison == 0){
+          comparison = card1.suit().compareTo(card2.suit());
+          if(comparison == 0){
+            comparison = -card1.rank().compareTo(card2.rank());
+          }
+        }
+        return comparison;
+      }
+    }
+
+
     //Create instance of Deck
     Deck deck = new Deck();
     System.out.println(deck);
@@ -27,24 +43,8 @@ public class Main {
     deck.sort(new ColorFirst());
     System.out.println(deck);
 
-
-
   }
 
-  private static class ColorFirst implements Comparator<Card> {
-
-    @Override
-    public int compare(Card card1, Card card2) {
-      int comparison = card1.suit().color().compareTo(card2.suit().color());
-      if (comparison == 0){
-        comparison = card1.suit().compareTo(card2.suit());
-        if(comparison == 0){
-          comparison = -card1.rank().compareTo(card2.rank());
-        }
-      }
-      return comparison;
-    }
-  }
 
 }
 

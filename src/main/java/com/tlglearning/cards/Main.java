c45aed155763137b02ccf09b153e7e5505160f47
skhadge <suchit.khadge@gmail.com>
2022-09-28 09:06:32 -0700

    Added lambda expression for sort comparator method

--- src/main/java/com/tlglearning/cards/Main.java
+++ src/main/java/com/tlglearning/cards/Main.java
@@ -26,19 +26,16 @@ public class Main {
     System.out.println(deck);
 
     //Sory by Color first
-    deck.sort(new Comparator<> (){    //Anonymous class
-
-      @Override
-      public int compare(Card card1, Card card2) {
-        int comparison = card1.suit().color().compareTo(card2.suit().color());
-        if (comparison == 0){
-          comparison = card1.suit().compareTo(card2.suit());
-          if(comparison == 0){
-            comparison = -card1.rank().compareTo(card2.rank());
-          }
+    //Anonymous class
+    deck.sort((card1, card2) -> {
+      int comparison = card1.suit().color().compareTo(card2.suit().color());
+      if (comparison == 0){
+        comparison = card1.suit().compareTo(card2.suit());
+        if(comparison == 0){
+          comparison = -card1.rank().compareTo(card2.rank());
         }
-        return comparison;
       }
+      return comparison;
     });
     System.out.println(deck);
 
